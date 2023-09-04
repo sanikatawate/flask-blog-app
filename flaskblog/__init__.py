a81891f9ee2b8707e9f172dfe6acf7404d24e2c5
sanikatawate <sanikatawate30@gmail.com>
2023-09-04 16:24:07 +0530

    Intial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sanika'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+with app.app_context():
+    db.create_all()
+
+from flaskblog import routes
+
